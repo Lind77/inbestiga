87667473860e41135b38b97cc51c1c19819b8a28
lindltaylor7 <riuyagami@gmail.com>
2023-03-13 08:43:47 -0500

    test deploy inbestiga

--- resources/js/components/sales/calendar/Home.vue
+++ resources/js/components/sales/calendar/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container-xxl flex-grow-1 container-p-y">
-      <h4 class="fw-bold py-3 mb-4"> Calendario</h4>
+      <h4 class="fw-bold py-3 mb-4"> Calendario o Test</h4>
       <div class="card p-4">
         <div class="row">
           <div class="col-4">
