b29f9e8b0c86af02d828bbbc4875ff1d643fe4a8
lindltaylor7 <riuyagami@gmail.com>
2023-03-14 17:17:57 -0500

    fix teams 2

--- resources/js/components/academic/projects/TeamModal.vue
+++ resources/js/components/academic/projects/TeamModal.vue
@@ -3,7 +3,7 @@
         <div class="modal-dialog modal-sm" role="document">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel2">Asignar equipo</h5>
+                <h5 class="modal-title" id="exampleModalLabel2">Asignar equipos</h5>
                 <button
                 type="button"
                 class="btn-close"
