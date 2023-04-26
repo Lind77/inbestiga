d7743aa12c967e0d3c135de9d7b898a07e7ea371
lindltaylor7 <riuyagami@gmail.com>
2023-04-26 09:36:20 -0500

    upd quot file

--- resources/js/components/sales/quotations/quotationFile.vue
+++ resources/js/components/sales/quotations/quotationFile.vue
@@ -92,7 +92,7 @@
                     <th class="text-purple sugested py-3 ps-1" colspan="3">PRODUCTO SUGERIDO</th>
                 </tr>
                 <tr v-for="detail in quotation[0].details" class="text-dark">
-                    <th class="table-item fw-normal ps-2" v-if="detail.type == 2 ">{{detail.product.title}}</th>
+                    <th class="table-item fw-normal ps-2" v-if="detail.type == 2 ">{{detail.new_product.name}}</th>
                     <th class="table-item fw-normal" v-if="detail.type == 2 " style="white-space: pre;">{{detail.description}}</th>
                     <th class="table-item fw-normal" v-if="detail.type == 2 ">S./{{detail.price}}</th>
                 </tr>
