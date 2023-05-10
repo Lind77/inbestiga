2a6ddfe0967caa3a8edf72f88beda7cfce3c4981
lindltaylor7 <riuyagami@gmail.com>
2023-05-10 16:20:03 -0500

    update minimum

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link :to="{name: 'main-acad'}">Área académica</router-link>
+    <router-link :to="{name: 'main-acad'}">Área académicas</router-link>
     <br>
     <router-link :to="{name: 'main-sales'}">Área de Ventas</router-link>
 </template>
