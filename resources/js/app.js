24bfaa6bb4634ebf442719e15cf3da6d92cd6715
lindltaylor7 <riuyagami@gmail.com>
2023-05-11 12:04:45 -0500

    upd app3

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,7 +32,7 @@ function loggedIn(){
 
 //Set the router
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory('/public'),
     routes,
     linkActiveClass: 'active'
 })
