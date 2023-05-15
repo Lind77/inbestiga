45c191834d0a15d85aa4276af707c6aa9b2e9159
lindltaylor7 <riuyagami@gmail.com>
2023-05-15 16:59:13 -0500

    uppdate add

--- resources/js/components/sales/funnel/FunnelModal.vue
+++ resources/js/components/sales/funnel/FunnelModal.vue
@@ -78,7 +78,12 @@ export default {
     data(){
         return{
             showOptionOwner:false,
-            newOwner:1
+            newOwner:1,
+            typeComunication:{
+                1: 'LLamar',
+                2: 'Escribir',
+                3: 'Meet'
+            }
         }
     },
     props:{
