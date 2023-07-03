ce982cc83311268e77de91ce71ecc37edcc31814
lindltaylor7 <riuyagami@gmail.com>
2023-07-03 17:50:07 -0500

    fix contract

---
+++ resources/js/components/experience/calendly/OffCanvas.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasEvent" aria-labelledby="offcanvasEndLabel">
+        <div class="offcanvas-header" v-if="info.event">
+            <h3 id="offcanvasEndLabel" class="offcanvas-title">Evento {{ info.event.title }}</h3>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body my-auto mx-0 flex-grow-0">
+            <p class="text-primary h5"> (Equipo)</p>
+            <h5 @dblclick="pickDeadline" class="text-danger cursor-pointer">Fecha de Culminación:</h5>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        info: Object
+    }
+}
+</script>
+<style lang="">
+    
+</style>
