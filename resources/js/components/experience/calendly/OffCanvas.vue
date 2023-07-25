7912b071eda0068a7ebae301839dc63c41defeef
lindltaylor7 <riuyagami@gmail.com>
2023-07-25 08:49:21 -0500

    fix calendar meetings offcanvas

--- resources/js/components/experience/calendly/OffCanvas.vue
+++ resources/js/components/experience/calendly/OffCanvas.vue
@@ -4,10 +4,11 @@
             <h3 id="offcanvasEndLabel" class="offcanvas-title">Evento {{ info.event.title }}</h3>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-
-        <div class="offcanvas-body mx-0 flex-grow-0">
+        <div class="offcanvas-body mx-0 flex-grow-0" v-if="info.event">
             <h5 @dblclick="pickDeadline">Comentario: {{ info.event.extendedProps.comment }}</h5>
             <p class="text-danger cursor-pointer">Fecha: {{ formatDate(info.event.start) }}</p>
+            <button v-if="info.event.extendedProps.link" class="btn btn-primary"
+                :href="info.event.extendedProps.link">Reunión Meet</button>
         </div>
     </div>
 </template>
