16837552adde6ab49a32adc97889af7200c83ce4
Julio Eduardo <julio.edward.fabian@gmial.com>
2023-08-04 10:46:58 -0500

    feat migration contract

--- app/Models/Work_contract.php
+++ app/Models/Work_contract.php
@@ -8,11 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Work_contract extends Model
 {
     use HasFactory;
-    protected $fillable = ['clausule', 'salary'];
+    protected $fillable = ['clausule', 'salary','type','status','user_id'];
 
-    //relacion uno a muchos user-comissions
-    public function user()
-    {
+    //relacion uno a uno inverso user-work_contract
+    public function user(){
         return $this->belongsTo('App\Models\User');
     }
+    //relacion uno a muchos work_contract-clausules
+   public function clausules(){
+    return $this->hasMany('App\Models\Clausule');
+   }
 }
+
+
+
+
+
---
+++ database/migrations/2023_08_02_113035_add_fields_to_work_contracts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('work_contracts', function (Blueprint $table) {
+            $table->tinyInteger('type')->nullable();
+            $table->tinyInteger('status')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('work_contracts', function (Blueprint $table) {
+            //
+        });
+    }
+};
