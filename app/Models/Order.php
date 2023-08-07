f4547a230522d0a78997024de6c22bfa6620a4d4
Julio Eduardo <julio.edward.fabian@gmial.com>
2023-08-07 18:26:26 -0500

    realcion polimorfica comprobante de pagos-orden-contrato-payments

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,24 +12,23 @@ class Order extends Model
     protected $fillable = ['quotation_id', 'final_delivery', 'observations', 'suggested'];
 
     //Relacion inversa uno  a uno Order-Quotation
-    public function quotation()
-    {
+    public function quotation(){
         return $this->belongsTo('App\Models\Quotation');
     }
     //Relacion morfeable uno a muchos (O-C-PAYMENT)
-    public function payments()
-    {
+    public function payments(){
         return $this->morphMany('App\Models\Payment', 'paymentable');
     }
-
     //Relacion morfeable uno a muchos order,contract-deliveries
-    public function deliveries()
-    {
+    public function deliveries(){
         return $this->morphMany('App\Models\Delivery', 'deliverable');
     }
     //Relacion polimorfica uno a mucho addendum-order-contract
-    public function addendums()
-    {
+    public function addendums(){
         return $this->morphMany('App\Models\Addendum', 'addendumable');
     }
+   //Relacion morfeable uno a muchos (orders-contract-payments-payment_proofs)
+   public function payment_proofs(){
+    return $this->morphMany('App\Models\Payment_proof', 'payment_proofable');
+}
 }
