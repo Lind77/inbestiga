99c62939c6277250d4ab22385e2422b97a45f50c
lindltaylor7 <riuyagami@gmail.com>
2023-08-07 19:02:54 -0500

    feature generate voucher

--- app/Models/Payment_proof.php
+++ app/Models/Payment_proof.php
@@ -8,13 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Payment_proof extends Model
 {
     use HasFactory;
-    protected $fillable = ['payment_proofable_id','payment_proofableable_type','customer_id','code','date','subtotal','tax','discount','total_amount','type'];
+    protected $fillable = ['payment_proofable_id', 'payment_proofable_type', 'customer_id', 'code', 'date', 'subtotal', 'tax', 'discount', 'total_price', 'type'];
     //Relacion uno a muchos payment_proofs-proof_details
-    public function proof_details(){                            
-     return $this->hasMany('App\Models\Proof_detail');
+    public function proof_details()
+    {
+        return $this->hasMany('App\Models\Proof_detail');
     }
     //Relacion polimorfica uno a muchos unversa payments_(módulo de venta d eproductos o cursos online)-payment_proofs
-    public function payment_proofable(){
+    public function payment_proofable()
+    {
         return $this->morphTo();
-    }  
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo('App\Models\Customer');
+    }
 }
