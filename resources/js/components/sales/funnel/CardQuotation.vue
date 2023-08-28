0f26dd772f7e10ff064bb2654ef312c736f0160a
lindltaylor7 <riuyagami@gmail.com>
2023-08-28 16:54:01 -0500

    fix cards

--- resources/js/components/sales/funnel/CardQuotation.vue
+++ resources/js/components/sales/funnel/CardQuotation.vue
@@ -38,7 +38,7 @@ export default {
     },
     methods: {
         formatCant(number) {
-            return parseDouble(number).toFixed(1)
+            return parseFloat(number).toFixed(1)
         },
         showModalFunnel(quotation) {
             console.log(quotation);
