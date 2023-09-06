55e258ddbc40e4edbfde431f9c4f44009aa6cc3f
lindltaylor7 <riuyagami@gmail.com>
2023-09-06 10:49:57 -0500

    fix contract

--- resources/views/contract.blade.php
+++ resources/views/contract.blade.php
@@ -284,7 +284,7 @@
                 </ul>
                {{--  <div class="page-break"></div> --}}
                 <span>QUINTO: ENTREGAS Y FORMA DE ENTREGAS.</span>
-                <p style="margin-top: -10px;">Las entregas que EL LOCADOR otorgará a favor de EL CLIENTE serán cargadas al correo y/o grupo de WhatsApp creado en los siguientes términos: </p>
+                <p style="margin-top: -8px;">Las entregas que EL LOCADOR otorgará a favor de EL CLIENTE serán cargadas al correo y/o grupo de WhatsApp creado en los siguientes términos: </p>
                 <table class="delivery-table">
                     <thead>
                         <tr>
@@ -303,7 +303,7 @@
                     </tbody>
                 </table>
                  @if($contract->fifth_article == 1)
-                    <p>Además, EL LOCADOR se compromete con entregar a favor de EL CLIENTE los siguientes beneficios adicionales:
+                    <p style="margin-top: -5px;">Además, EL LOCADOR se compromete con entregar a favor de EL CLIENTE los siguientes beneficios adicionales:
                     </p>
                     <ul style="margin-top: -8px;">
                         <li>Asesoría de preparación metodológica y temática sobre la investigación para la sustentación.</li>
