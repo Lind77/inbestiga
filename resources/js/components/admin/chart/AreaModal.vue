8f1aa4c5c835c6646fed9efea8da7c3b43bc2cef
lindltaylor7 <riuyagami@gmail.com>
2023-10-10 08:45:44 -0500

    fix attendances permits

---
+++ resources/js/components/admin/chart/AreaModal.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="modal fade" id="areaModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="btn-close btn-pinned" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h4>Nueva área</h4>
+                    <div class="mb-3">
+                        <label for="form-label">Nombre</label>
+                        <input type="text" name="" id="" class="form-control">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
