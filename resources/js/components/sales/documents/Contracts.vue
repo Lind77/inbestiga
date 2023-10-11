66411160ca68bf8d7ce1dafca5b48cb53a068a4b
lindltaylor7 <riuyagami@gmail.com>
2023-10-11 12:44:16 -0500

    fix funnel

--- resources/js/components/sales/documents/Contracts.vue
+++ resources/js/components/sales/documents/Contracts.vue
@@ -17,7 +17,7 @@
             </td>
             <td>{{ contract.date ? contract.date : '' }}</td>
             <td>
-              <a :href="`http://localhost/inbestiga/public/api/generateContract/${contract.id}`" target="_blank"
+              <a :href="`https://sistema.inbestiga.com/public/api/generateContract/${contract.id}`" target="_blank"
                 class="btn btn-sm btn-primary mx-2 text-white" disabled><i class='bx bx-printer'></i></a>
             </td>
           </tr>
