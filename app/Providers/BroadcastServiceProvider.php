385e2d7bcf1844011f24a6098ee0ac35135fa7f8
lindltaylor7 <riuyagami@gmail.com>
2023-10-27 15:07:13 -0500

    fix com

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes(['middleware' => 'auth:api']);
+        Broadcast::routes(['middleware' => 'auth:sanctum']);
 
         require base_path('routes/channels.php');
     }
