1ab1109097d8fff714b326385a2d3becdee495be
JulioEduardoFabian <julio.edward.fabian@gmail.com>
2023-11-28 10:50:18 -0500

    justification migration

--- app/Models/Justification.php
+++ app/Models/Justification.php
@@ -8,9 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Justification extends Model
 {
     use HasFactory;
-    protected $fillable = ['miss_date', 'admission_time', 'departure_time', 'reason','status','user_id'];
-    //Relación  uno a muchos inverso User_Justifications
-    public function  user(){
+    protected $fillable = ['miss_date', 'miss_time_admission', 'miss_time_departure', 'reason','status','user_id','time_amount','left_time'];
+    //Relación  uno a muchos inverso attendances-Attendance-permit
+    public function user(){
         return $this->belongsTo('App\Models\User');
+    }
+    //Relación uno a muchos Attendance_permit-Recovery_dates
+    public function recovery_dates(){
+        return $this->hasMany('App\Models\Recovery_date');
     }
 }
