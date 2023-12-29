c7b9dd04580029f4b5ac4a9e62f4c9c8eddaa3b5
lindltaylor7 <riuyagami@gmail.com>
2023-12-29 12:06:17 -0500

    kanban upd

--- app/Models/Attendance_permit.php
+++ app/Models/Attendance_permit.php
@@ -8,16 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance_permit extends Model
 {
     use HasFactory;
-    protected $fillable = ['status', 'attendance_id', 'miss_date', 'miss_time_admission', 'miss_time_departure', 'reason', 'time_amount','left_amount','user_id'];
+    protected $fillable = ['status', 'attendance_id', 'miss_date', 'miss_time_admission', 'miss_time_departure', 'reason', 'time_amount', 'left_time', 'user_id'];
     //Relación  uno a muchos inverso attendances-Attendance-permit
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo('App\Models\User');
     }
     //Relación uno a muchos Attendance_permit-Recovery_dates
-    public function recovery_dates(){
-        return $this->hasMany('App\Models\Recovery_date');
+    public function recovery_dates()
+    {
+        return $this->morphMany('App\Models\Recovery_date', 'recoverable');
     }
-   /*  //Relacion morfeable uno a muchos (Justification-Attendance_permits-Recovery_dates)
+    /*  //Relacion morfeable uno a muchos (Justification-Attendance_permits-Recovery_dates)
     public function recovery_dates()
     {
         return $this->morphMany('App\Models\Recovery_date','recoverable');
