1a208ed66ded2b004381ec5a95e5cf603fd1f775
lindltaylor7 <riuyagami@gmail.com>
2024-01-05 13:16:03 -0500

    fix chat images

--- resources/js/components/admin/chat/ContactList.vue
+++ resources/js/components/admin/chat/ContactList.vue
@@ -33,7 +33,7 @@
                 :key="index" @click="selectContact(contact, index)">
                 <div class="d-flex justify-content-start overflow-hidden">
                     <div class="flex-shrink-0 avatar ms-2">
-                        <div class="flex-shrink-0 me-3" v-if="contact.images[0]">
+                        <div class="flex-shrink-0 me-3" v-if="contact.images && contact.images[0]">
                             <div class="avatar avatar-online">
                                 <img :src="'../../public/files/' + contact.images[0].url"
                                     class="w-px-40 h-px-40 rounded-circle" style="object-fit:cover;" />
