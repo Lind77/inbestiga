48d2014495d92e25357edf7c069d00368644e0dc
JulioEduardoFabian <julio.edward.fabian@gmail.com>
2024-01-09 09:55:43 -0500

    bd de feature points

--- app/Models/Academic_level.php
+++ app/Models/Academic_level.php
@@ -8,9 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Academic_level extends Model
 {
     use HasFactory;
-    protected $fillable=['name'];
+    protected $fillable=['name','category_id'];
     //Relacion muchos  a muchos academic_product-academic_level
     public function academic_products(){
         return $this->belongsToMany('App\Models\Academic_product','academic_level_academic_product');
+    }
+    //Relación inversa uno a muchos Category-Project
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
+    //Relacion uno a muchos Category-Projects
+    public function projects(){
+        return $this->hasMany('App\Models\Project');
     }
 }
