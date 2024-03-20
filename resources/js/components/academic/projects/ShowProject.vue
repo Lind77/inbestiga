e4c12ae269fc136ba7e8d486c0e7f2360e590c76
lindltaylor7 <riuyagami@gmail.com>
2024-03-20 08:01:41 -0500

    ux project

--- resources/js/components/academic/projects/ShowProject.vue
+++ resources/js/components/academic/projects/ShowProject.vue
@@ -2,28 +2,33 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="card w-100">
             <div class="card-body d-flex">
-                <img src="https://inbestiga.com/wp-content/uploads/2023/10/cropped-logo-Inbestiga.png" class=" rounded"
-                    width="100" height="100">
+                <img src="https://inbestiga.com/inbestiga/public/files/1709671609.png" class=" rounded" width="200"
+                    height="200">
                 <div class="ps-3 w-100">
                     <div class="d-flex justify-content-between">
                         <div>
-                            <h3>{{ project.title }}</h3>
+                            <h3 class="fw-bold mb-0">{{ project.title }}</h3>
+                            <span class="badge bg-success mb-3">{{ project.status }}</span>
                             <template v-if="project.projectable">
-                                <p class="text-dark fw-bold"
-                                    v-for="customer in project.projectable.quotation.customers">{{
-                                customer.name }}</p>
+                                <div class="d-flex align-items-center avatar-group my-3">
+                                    <div class="avatar avatar-sm me-2"
+                                        v-for="customer in project.projectable.quotation.customers"
+                                        :title="customer.name">
+                                        <span class="avatar-initial rounded-circle bg-primary">{{ customer.name[0]
+                                            }}</span>
+                                    </div>
+                                </div>
                             </template>
                         </div>
-                        <button class="btn btn-success h-25 btn-small">{{ project.status }}</button>
                     </div>
 
                     <button class="btn btn-success me-2">
                         <p class="mb-0">Fecha de inicio</p>
-                        {{ project.created_at }}
+                        {{ formatDate(project.created_at) }}
                     </button>
                     <button class="btn btn-danger me-2">
                         <p class="mb-0">Fecha de entrega</p>
-                        {{ project.deadline }}
+                        {{ project.deadline ? formatDate(project.deadline) : '-' }}
                     </button>
                     <button class="btn btn-info me-2" v-if="project.projectable">
                         <p class="mb-0">Presupuesto</p>
@@ -38,9 +43,28 @@
         </div>
         <div class="card w-100">
             <div class="card-body d-flex justify-content-between">
-                <div>
-
-                </div>
+                <ul class="nav nav-pills mb-3" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-top-home" aria-controls="navs-pills-top-home"
+                            aria-selected="true">Tabla</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-top-sprints" aria-controls="navs-pills-top-sprints"
+                            aria-selected="false" tabindex="-1">Sprints</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-top-profile" aria-controls="navs-pills-top-profile"
+                            aria-selected="false" tabindex="-1">Kanban</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-top-calendar" aria-controls="navs-pills-top-calendar"
+                            aria-selected="false" tabindex="-1">Calendario</button>
+                    </li>
+                </ul>
                 <div>
                     <button class="btn btn-danger mx-1" @click="showModalFiles"><i
                             class='bx bx-file'></i>Archivos</button>
@@ -51,28 +75,7 @@
             </div>
         </div>
         <div class="nav-align-top mb-4 pt-2 ">
-            <ul class="nav nav-pills mb-3" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
-                        data-bs-target="#navs-pills-top-home" aria-controls="navs-pills-top-home"
-                        aria-selected="true">Tabla</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
-                        data-bs-target="#navs-pills-top-sprints" aria-controls="navs-pills-top-sprints"
-                        aria-selected="false" tabindex="-1">Sprints</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
-                        data-bs-target="#navs-pills-top-profile" aria-controls="navs-pills-top-profile"
-                        aria-selected="false" tabindex="-1">Kanban</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
-                        data-bs-target="#navs-pills-top-calendar" aria-controls="navs-pills-top-calendar"
-                        aria-selected="false" tabindex="-1">Calendario</button>
-                </li>
-            </ul>
+
             <div class="tab-content">
                 <div class="tab-pane fade active show" id="navs-pills-top-home" role="tabpanel">
                     <p>Tabla</p>
@@ -121,6 +124,9 @@ export default {
         }
     },
     methods: {
+        formatDate(date) {
+            return moment(date).format('DD/MM/YYYY')
+        },
         updateFilesModal() {
             this.getProject()
         },
