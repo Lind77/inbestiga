93241da3ba61802f297f09bbb55c50812d131e51
Julio Fabian <julio.edward.fabian@gmail.com>
2024-05-16 18:11:33 -0500

    bd post-ficha relacionados projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,7 +9,7 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['projectable_id', 'projectable_type', 'title', 'team_id', 'user_id', 'status','start_date', 'deadline', 'academic_date', 'aditional', 'academic_level_id'];
+    protected $fillable = ['projectable_id', 'projectable_type', 'title', 'team_id', 'user_id', 'status','start_date', 'deadline', 'academic_date', 'aditional', 'academic_level_id','type'];
 
     //Relacion  inversa morfeable uno a muchos order,contract-project
     public function projectable()
@@ -30,6 +30,10 @@ class Project extends Model
     public function deliveries()
     {
         return $this->hasMany("App\Models\Delivery");
+    }
+    //Relación uno a muchos Project_Post
+    public function posts(){
+        return $this->hasMany("App\Models\Post");
     }
     //Relacion muchos a muchos project_startupactivity
     public function startup_activities()
---
+++ database/migrations/2024_05_16_133149_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('postable_id');
+            $table->string('postable_type');
+            $table->string('title')->nullable();
+            $table->text('body')->nullable();
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->foreign('project_id')->references('id')->on('projects')->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
