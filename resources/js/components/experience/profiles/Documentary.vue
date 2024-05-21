ac21d060d2575a7124b5ad4e1aa349882f0c3053
lindltaylor7 <riuyagami@gmail.com>
2024-05-21 18:55:46 -0500

    upd documentary line

---
+++ resources/js/components/experience/profiles/Documentary.vue
@@ -0,0 +1,35 @@
+<template lang="">
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <template v-for="newQuestion in questions">
+                        <td v-if="newQuestion.type == 2">
+                            {{ newQuestion.question }}
+                        </td>
+                    </template>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <template v-for="newQuestion in questions">
+                        <td v-if="newQuestion.type == 2">
+                            <input
+                                type="checkbox"
+                                class="form-check-input"
+                                v-model="newQuestion.answer"
+                            />
+                        </td>
+                    </template>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        questions: Array,
+    },
+};
+</script>
