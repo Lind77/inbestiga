91762a8d7ceed433fc9b3ad34074b81720d32aa6
Julio Fabian <julio.edward.fabian@gmail.com>
2024-06-20 15:39:01 -0500

    project_situation en properties

---
+++ database/migrations/2024_06_20_151430_add_fields_to_properties.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_situation_id')->nullable();
+            $table->foreign('project_situation_id')->references('id')->on('project_situations')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->dropForeign(['project_situation_id']);
+            $table->dropColumn('project_situation_id');  
+        });
+    }
+};
