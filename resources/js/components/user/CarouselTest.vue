e46dbdf08ef9bb5230f57e942345c1aa7300b829
lindltaylor7 <riuyagami@gmail.com>
2024-07-08 12:21:55 -0500

    videos

--- resources/js/components/user/CarouselTest.vue
+++ resources/js/components/user/CarouselTest.vue
@@ -61,79 +61,79 @@ export default {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/1.png",
                     text: "Bienvenida",
-                    link: "https://youtu.be/P3H5DwGXUh8",
+                    link: "https://www.youtube.com/embed/P3H5DwGXUh8",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/2.png",
                     text: "Canal oficial de comunicación",
-                    link: "https://youtu.be/6Me92Ga7SfI",
+                    link: "https://www.youtube.com/embed/6Me92Ga7SfI",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/3.png",
                     text: "Leer con atención el contrato",
-                    link: "https://youtu.be/_kmmTqjIOtQ",
+                    link: "https://www.youtube.com/embed/_kmmTqjIOtQ",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/4.png",
                     text: "Horario de atención",
-                    link: "https://youtu.be/fff_oGT3wvg",
+                    link: "https://www.youtube.com/embed/fff_oGT3wvg",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/5.png",
                     text: "Reuniones cuando se requiera",
-                    link: "https://youtu.be/O1h-PK20vcM",
+                    link: "https://www.youtube.com/embed/O1h-PK20vcM",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/6.png",
                     text: "Todos los archivos deben ser enviados a través del formulario",
-                    link: "https://youtu.be/dOHOa2XGJ4M",
+                    link: "https://www.youtube.com/embed/dOHOa2XGJ4M",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/7.png",
                     text: "Trabajos serán descargados a través del aula",
-                    link: "https://youtu.be/TF_TKaFo8NM",
+                    link: "https://www.youtube.com/embed/TF_TKaFo8NM",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/8.png",
                     text: "Toda descarga de archivos es validada primero por el pago",
-                    link: "https://youtu.be/zPGjQkIJ_M0",
+                    link: "https://www.youtube.com/embed/zPGjQkIJ_M0",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/9.png",
                     text: "Levantamiento de observaciones tiene un plazo de 7 días hábiles ",
-                    link: "",
+                    link: "https://www.youtube.com/embed/upDmBlrd1is",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/10.png",
                     text: "Si necesitas en menos tiempo, agenda una reunión y coméntalo allí ",
-                    link: "https://youtu.be/upDmBlrd1is",
+                    link: "https://www.youtube.com/embed/758Y_RGou7Y",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/11.png",
                     text: "Solo se levantan observaciones de revisores académicos de la universidad ",
-                    link: "https://youtu.be/758Y_RGou7Y",
+                    link: "https://www.youtube.com/embed/42eepglyqGI",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/12.png",
                     text: "Se te anexará un informe de levantamiento de observaciones para verificar los cambios ",
-                    link: "https://youtu.be/42eepglyqGI",
+                    link: "https://www.youtube.com/embed/uN6UrUNGwk0",
                 },
                 {
                     imageUrl:
                         "https://inbestiga.com/inbestiga/public/img/13.png",
                     text: "Antes de reunirte con tu asesor, revisa el documento a detalle",
-                    link: "https://youtu.be/uN6UrUNGwk0",
+                    link: "https://www.youtube.com/embed/DR9ZJ53uqO8",
                 },
             ],
             linkSelected: "",
--- resources/js/components/user/VideoModal.vue
+++ resources/js/components/user/VideoModal.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="modal fade" id="videoModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
-                <div class="modal-body">
-                    <iframe class="w-100 h-100" :src="videoSrc"> </iframe>
+                <div class="modal-body p-0">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe
+                            class="embed-responsive-item w-100 vh-75"
+                            :src="videoSrc"
+                            allowfullscreen
+                        ></iframe>
+                    </div>
                 </div>
             </div>
         </div>
