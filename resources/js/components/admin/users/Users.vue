1a1cd83b9d11cfba6913b2ebe715ec63bc8d0c55
lindltaylor7 <riuyagami@gmail.com>
2024-09-12 15:50:15 -0500

    schedules

--- resources/js/components/admin/users/Users.vue
+++ resources/js/components/admin/users/Users.vue
@@ -1,10 +1,22 @@
 <template>
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="card">
-            <h5 class="card-header d-flex align-items-center justify-content-between">Usuarios
-                <button @click="addUser" class="btn btn-secondary add-new btn-primary w-25" type="button"><span><i
-                            class="bx bx-plus me-0 me-sm-1"></i><span class="d-none d-sm-inline-block">Nuevo
-                            Usuario</span></span></button>
+            <h5
+                class="card-header d-flex align-items-center justify-content-between"
+            >
+                Usuarios
+                <button
+                    @click="addUser"
+                    class="btn btn-secondary add-new btn-primary w-25"
+                    type="button"
+                >
+                    <span
+                        ><i class="bx bx-plus me-0 me-sm-1"></i
+                        ><span class="d-none d-sm-inline-block"
+                            >Nuevo Usuario</span
+                        ></span
+                    >
+                </button>
             </h5>
 
             <div class="table-responsive text-nowrap">
@@ -20,21 +32,34 @@
                         </tr>
                     </thead>
                     <tbody class="table-border-bottom-0">
-                        <tr v-for="user in  users ">
+                        <tr v-for="user in users">
                             <td>{{ user.id }}</td>
                             <td>{{ user.name }}</td>
-                            <td>{{ user.subarea.area.name }}</td>
-                            <td v-if="user.roles[0]">{{ user.roles[0].name }}</td>
+                            <td>
+                                {{ user.subarea ? user.subarea.area.name : "" }}
+                            </td>
+                            <td v-if="user.roles[0]">
+                                {{ user.roles[0].name }}
+                            </td>
                             <td>{{ user.email }}</td>
                             <td>
-                                <span v-if="user.id != 1" @click="deleteUser(user.id)"
-                                    class="badge bg-label-danger me-1 cursor-pointer"><i class='bx bx-trash'></i>
+                                <span
+                                    v-if="user.id != 1"
+                                    @click="deleteUser(user.id)"
+                                    class="badge bg-label-danger me-1 cursor-pointer"
+                                    ><i class="bx bx-trash"></i>
                                 </span>
-                                <span v-if="user.id != 1" class="badge bg-label-primary me-1 cursor-pointer"
-                                    @click="showPermissionModal(user)"><i class='bx bx-dialpad-alt'></i>
+                                <span
+                                    v-if="user.id != 1"
+                                    class="badge bg-label-primary me-1 cursor-pointer"
+                                    @click="showPermissionModal(user)"
+                                    ><i class="bx bx-dialpad-alt"></i>
                                 </span>
-                                <span v-if="user.id != 1" class=" badge bg-label-primary me-1 cursor-pointer"
-                                    @click="showRoleUpdate(user)"><i class='bx bx-edit'></i>
+                                <span
+                                    v-if="user.id != 1"
+                                    class="badge bg-label-primary me-1 cursor-pointer"
+                                    @click="showRoleUpdate(user)"
+                                    ><i class="bx bx-edit"></i>
                                 </span>
                             </td>
                         </tr>
@@ -48,70 +73,70 @@
     </div>
 </template>
 <script>
-import InsertUser from './InsertUser.vue'
-import PermissionsModal from './PermissionsModal.vue'
-import RoleModal from './RoleModal.vue'
+import InsertUser from "./InsertUser.vue";
+import PermissionsModal from "./PermissionsModal.vue";
+import RoleModal from "./RoleModal.vue";
 export default {
     components: { InsertUser, PermissionsModal, RoleModal },
     data() {
         return {
             users: [],
             area: {
-                'admin': 'Administración',
-                'sales': 'Ventas',
-                'acad': 'Académico',
-                'experience': 'Experiencia del Inbestigador'
+                admin: "Administración",
+                sales: "Ventas",
+                acad: "Académico",
+                experience: "Experiencia del Inbestigador",
             },
-            userSelected: {}
-        }
+            userSelected: {},
+        };
     },
     methods: {
         showRoleUpdate(user) {
-            this.userSelected = user
-            $('#roleModal').modal('show');
+            this.userSelected = user;
+            $("#roleModal").modal("show");
         },
         showPermissionModal(user) {
-            $('#modalPermission').modal('show');
-            this.userSelected = user
+            $("#modalPermission").modal("show");
+            this.userSelected = user;
         },
         getAllUsers() {
-            axios.get('/api/users')
-                .then(res => {
-                    this.users = res.data
+            axios
+                .get("/api/users")
+                .then((res) => {
+                    this.users = res.data;
                 })
-                .catch(err => {
-                    console.log(err)
-                })
+                .catch((err) => {
+                    console.log(err);
+                });
         },
         addUser() {
-            $('#userModal').modal('show')
+            $("#userModal").modal("show");
         },
         deleteUser(id) {
             this.$swal({
-                title: '¿Deseas eliminar a este usuario?',
-                icon: 'question',
-                confirmButtonText: 'Si',
+                title: "¿Deseas eliminar a este usuario?",
+                icon: "question",
+                confirmButtonText: "Si",
                 showCancelButton: true,
-                cancelButtonText: 'No'
-            })
-                .then((result) => {
-                    if (result.isConfirmed) {
-                        axios.delete(`/api/users/${id}`)
-                            .then(res => {
-                                this.getAllUsers()
-                            })
-                            .catch(err => {
-                                console.log(err)
-                            })
-                    } else {
-                        this.$swal.close()
-                    }
-                })
-
-        }
+                cancelButtonText: "No",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    axios
+                        .delete(`/api/users/${id}`)
+                        .then((res) => {
+                            this.getAllUsers();
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                } else {
+                    this.$swal.close();
+                }
+            });
+        },
     },
     mounted() {
-        this.getAllUsers()
-    }
-}
+        this.getAllUsers();
+    },
+};
 </script>
--- resources/js/components/experience/schedules/Hour.vue
+++ resources/js/components/experience/schedules/Hour.vue
@@ -1,34 +1,37 @@
 <template>
-    <button @dblclick="showModalHour(schedule)" @click="disableHour"
-        :class="`btn btn-${colorByType[schedule.type]} p-2 mb-1 w-100`">{{
-            formatTime(schedule.admission_time) }} - {{
-        formatTime(schedule.departure_time)
-    }}</button>
+    <button
+        @dblclick="showModalHour(schedule)"
+        @click="disableHour"
+        :class="`btn btn-${colorByType[schedule.type]} p-2 mb-1 w-100`"
+    >
+        {{ formatTime(schedule.admission_time) }} -
+        {{ formatTime(schedule.departure_time) }}
+    </button>
 </template>
 <script>
-import { userStore } from '../../../stores/UserStore';
-import moment from 'moment';
+import { userStore } from "../../../stores/UserStore";
+import moment from "moment";
 export default {
     setup() {
-        const store = userStore()
-        return { store }
+        const store = userStore();
+        return { store };
     },
     data() {
         return {
             colorByType: {
-                1: 'success',
-                2: 'secondary',
-                3: 'warning'
+                1: "success",
+                2: "secondary",
+                3: "warning",
             },
-            scheduleSelected: {}
-        }
+            scheduleSelected: {},
+        };
     },
     props: {
-        schedule: Object
+        schedule: Object,
     },
     methods: {
         showModalHour(schedule) {
-            this.$emit('showModalHour', schedule)
+            this.$emit("showModalHour", schedule);
             /* if (this.store.authUser.id == 9) {
                 this.$emit('showModalHour', schedule)
             } else {
@@ -36,25 +39,27 @@ export default {
             } */
         },
         formatTime(time) {
-            return moment(time, "HH:mm:ss").format("hh:mm")
+            return moment(time, "HH:mm:ss").format("hh:mm a");
         },
         disableHour() {
             if (this.schedule.type == 1) {
-                this.schedule.type = 2
-                this.$emit('minusHour')
+                this.schedule.type = 2;
+                this.$emit("minusHour");
             } else {
-                this.schedule.type = 1
-                this.$emit('addHour')
+                this.schedule.type = 1;
+                this.$emit("addHour");
             }
 
-            const fd = new FormData()
-            fd.append('type', this.schedule.type)
-            fd.append('_method', 'put');
+            const fd = new FormData();
+            fd.append("type", this.schedule.type);
+            fd.append("_method", "put");
 
-            axios.post('/api/schedules-type/' + this.schedule.id, fd)
+            axios
+                .post("/api/schedules-type/" + this.schedule.id, fd)
                 .then((result) => {
-                    console.log(result)
-                }).catch((err) => {
+                    console.log(result);
+                })
+                .catch((err) => {
                     console.log(err);
                 });
             /* if (this.store.authUser.id == 9) {
@@ -62,8 +67,7 @@ export default {
             } else {
                 alert('Para actualizar su horario debe comunicarse con Experiencia')
             } */
-
-        }
-    }
-}
+        },
+    },
+};
 </script>
