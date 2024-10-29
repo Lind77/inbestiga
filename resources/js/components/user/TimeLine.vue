5d2ce8c292126d3acf522638fb511411e8170739
lindltaylor7 <riuyagami@gmail.com>
2024-10-29 10:57:07 -0500

    update justifications

--- resources/js/components/user/TimeLine.vue
+++ resources/js/components/user/TimeLine.vue
@@ -1,28 +1,29 @@
-<template lang="">
+<template>
     <div class="col-12 d-flex flex-column justify-content-center">
         <div
-            class="d-flex justify-content-around text-white button-multiple py-2"
+            class="d-flex flex-row justify-content-around text-white button-multiple py-2"
         >
             <div
                 @click="openModalAcademic"
-                class="d-flex flex-row justify-content-center align-items-center cursor-pointer"
+                class="d-flex w-50 flex-row justify-content-center align-items-center cursor-pointer"
             >
                 Información <br />
                 académica <i class="bx bx-book-open ms-3"></i>
             </div>
+            <div class="separator-vertical"></div>
             <div
                 @click="openModalFiles"
-                class="d-flex flex-row justify-content-center align-items-center cursor-pointer"
+                class="d-flex w-50 flex-row justify-content-center align-items-center cursor-pointer"
             >
                 Documentos <i class="bx bx-file ms-3"></i>
             </div>
-            <div
+            <!-- <div
                 @click="toCalendly"
                 class="d-flex flex-row justify-content-center align-items-center cursor-pointer"
             >
                 Agenda <br />
                 tu reunión <i class="bx bx-video-plus ms-3"></i>
-            </div>
+            </div> -->
         </div>
 
         <button class="btn bg-none w-100 mt-3" @click="openModalPost">
@@ -78,11 +79,42 @@ button {
     border: 1px solid #c908aa;
     color: #fff !important;
     font-weight: 500;
+    background-color: rgba(255, 255, 255, 0); /* Color sólido inicial */
+    transition: background-color 0.3s ease-in-out;
+    position: relative;
+    z-index: 1;
+}
+
+button::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+        25deg,
+        rgba(244, 21, 217, 1) 0%,
+        rgba(127, 33, 251, 1) 100%
+    );
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+    transition: opacity 0.8s ease-in-out;
+    left: 0;
+    top: 0;
+    border-radius: 5px;
+}
+
+button:hover::after {
+    opacity: 1;
 }
 
 .button-multiple {
     border: 1px solid #c908aa;
     border-radius: 5px;
     padding: 3px 5px 5px;
+}
+
+.separator-vertical {
+    background-color: #c908aa;
+    width: 1px;
 }
 </style>
