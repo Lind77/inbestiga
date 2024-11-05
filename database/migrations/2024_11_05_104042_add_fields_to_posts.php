6921958a30f493a361cc2a6439eee2c09f5e5c6b
Julio Fabian <julio.edward.fabian@gmail.com>
2024-11-05 11:50:54 -0500

    post_Type para notificaciones

---
+++ database/migrations/2024_11_05_104042_add_fields_to_posts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('post_type_id')->nullable();
+            $table->foreign('post_type_id')->references('id')->on('post_types')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['post_type_id']);
+            $table->dropColumn('post_type_id'); 
+        });
+    }
+};
