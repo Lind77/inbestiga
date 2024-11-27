b6425dc41bd6127db9e1f5455830dc1ea70c760f
lindltaylor7 <riuyagami@gmail.com>
2024-11-27 10:27:38 -0500

    update documentation

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -196,7 +196,7 @@ class QuotationController extends Controller
      */
     public function show($id)
     {
-        $quotation = Quotation::where('id', $id)->with(['customers', 'customers.province', 'customers.province.department', 'customers.province.department.provinces', 'customers.user', 'customers.comunications', 'details', 'details.product', 'order', 'contract', 'contract.projects', 'contract.projects.team', 'contract.projects.team.users', 'contract.properties', 'contract.projects.team.users.roles', 'contract.projects.files', 'contract.projects.posts' => function ($query) {
+        $quotation = Quotation::where('id', $id)->with(['customers', 'customers.province', 'customers.province.department', 'customers.province.department.provinces', 'customers.user', 'customers.comunications', 'details', 'details.product', 'order', 'contract', 'contract.user', 'contract.projects', 'contract.projects.team', 'contract.projects.team.users', 'contract.properties', 'contract.projects.team.users.roles', 'contract.projects.files', 'contract.projects.posts' => function ($query) {
             $query->orderBy('id', 'desc')->get();
         }, 'contract.projects.posts.postable', 'contract.projects.posts.files', 'contract.properties' => function ($query) {
             $query->orderBy('id', 'desc')->first();
