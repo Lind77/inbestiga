1cf752e2404e953c5a1bcfa2005f1bf2129acef7
digitechgrouperu <digitechgrouperu@gmail.com>
2025-02-17 19:18:30 -0500

    fix xlsx postsales

--- app/Exports/CustomersExport.php
+++ app/Exports/CustomersExport.php
@@ -13,39 +13,54 @@ class CustomersExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        return Customer::with(['quotations' => function ($query) {
-            $query->orderBy('id', 'desc');
-        }, 'quotations.contract', 'quotations.contract.post_form', 'quotations.contract.post_form.comunication_channel', 'quotations.contract.post_form.study_place', 'quotations.contract.post_form.marketing_source', 'quotations.contract.post_form.hire_factor', 'quotations.contract.post_form.contract_mode', 'quotations.contract.post_form.academic_situation', 'quotations.contract.post_form.professional_status', 'quotations.contract.post_form.participation', 'province'])
-            ->where('password', '!=', null)
+        return Customer::with([
+            'quotations' => function ($query) {
+                $query->orderBy('id', 'desc');
+            },
+            'quotations.contract.post_form.comunication_channel',
+            'quotations.contract.post_form.study_place',
+            'quotations.contract.post_form.marketing_source',
+            'quotations.contract.post_form.hire_factor',
+            'quotations.contract.post_form.contract_mode',
+            'quotations.contract.post_form.academic_situation',
+            'quotations.contract.post_form.professional_status',
+            'quotations.contract.post_form.participation',
+            'province'
+        ])
+            ->whereNotNull('password')
             ->whereHas('quotations.contract', function ($q) {
                 $q->where('registration_date', 'like', '%' . date('Y-m') . '%');
             })
             ->orderBy('updated_at', 'desc')
             ->get()
             ->map(function ($customer) {
+                $quotation = $customer->quotations->first();
+                $contract = optional($quotation)->contract;
+                $postForm = optional($contract)->post_form;
+
                 return [
-                    'Fecha de Registro del Contrato' => $customer->quotations->first()->contract->registration_date ?? 'N/A',
-                    'Código' => $customer->quotations->first()->contract->code ?? 'N/A',
+                    'Fecha de Registro del Contrato' => optional($contract)->registration_date ?? 'N/A',
+                    'Código' => optional($contract)->code ?? 'N/A',
                     'Nombre' => $customer->name,
                     'Correo Electrónico' => $customer->email,
                     'DNI' => $customer->dni,
                     'Sexo' => $customer->gender,
                     'Fecha de nacimiento' => $customer->birth_date ?? 'N/A',
                     'Celular' => $customer->cell,
-                    'Ciudad de Residencia' => $customer->province->name ?? 'N/A',
+                    'Ciudad de Residencia' => optional($customer->province)->name ?? 'N/A',
                     'Universidad' => $customer->university,
                     'Carrera' => $customer->career,
-                    'Tipo de Tesis' => $customer->quotations->first()->contract->thesis_type_id,
-                    'Grado' => $customer->quotations->first()->contract->thesis_degree_id,
-                    'Monto contratado' => $customer->quotations->first()->amount,
-                    'Canal de comunicación preferido' => $customer->quotations->first()->contract->post_form->comunication_channel ? $customer->quotations->first()->contract->post_form->comunication_channel->name : 'N/A',
-                    'Cuenta con lugar de estudio' => $customer->quotations->first()->contract->post_form->study_place ? $customer->quotations->first()->contract->post_form->study_place->name : 'N/A',
-                    '¿Cómo te enteraste de nuestros servicios?' => $customer->quotations->first()->contract->post_form->marketing_source ? $customer->quotations->first()->contract->post_form->marketing_source->name : 'N/A',
-                    '¿Cuál fue el factor principal que te llevó a contratar nuestros servicios?' => $customer->quotations->first()->contract->post_form->hire_factor ? $customer->quotations->first()->contract->post_form->hire_factor->name : 'N/A',
-                    '¿Cómo realizaste la contratación de nuestros servicios?' => $customer->quotations->first()->contract->post_form->contract_mode ? $customer->quotations->first()->contract->post_form->contract_mode->name : 'N/A',
-                    '¿Cuál es tu situación académica actual?' => $customer->quotations->first()->contract->post_form->academic_situation ? $customer->quotations->first()->contract->post_form->academic_situation->name : 'N/A',
-                    'Estado profesional' => $customer->quotations->first()->contract->post_form->professional_status ? $customer->quotations->first()->contract->post_form->professional_status->name : 'N/A',
-                    '¿Qué tanto deseas participar en la elaboración de tu tesis?' => $customer->quotations->first()->contract->post_form->participation ? $customer->quotations->first()->contract->post_form->participation->name : 'N/A'
+                    'Tipo de Tesis' => optional($contract)->thesis_type_id ?? 'N/A',
+                    'Grado' => optional($contract)->thesis_degree_id ?? 'N/A',
+                    'Monto contratado' => optional($quotation)->amount ?? 'N/A',
+                    'Canal de comunicación preferido' => optional($postForm)->comunication_channel->name ?? 'N/A',
+                    'Cuenta con lugar de estudio' => optional($postForm)->study_place->name ?? 'N/A',
+                    '¿Cómo te enteraste de nuestros servicios?' => optional($postForm)->marketing_source->name ?? 'N/A',
+                    '¿Cuál fue el factor principal que te llevó a contratar nuestros servicios?' => optional($postForm)->hire_factor->name ?? 'N/A',
+                    '¿Cómo realizaste la contratación de nuestros servicios?' => optional($postForm)->contract_mode->name ?? 'N/A',
+                    '¿Cuál es tu situación académica actual?' => optional($postForm)->academic_situation->name ?? 'N/A',
+                    'Estado profesional' => optional($postForm)->professional_status->name ?? 'N/A',
+                    '¿Qué tanto deseas participar en la elaboración de tu tesis?' => optional($postForm)->participation->name ?? 'N/A'
                 ];
             });
     }
